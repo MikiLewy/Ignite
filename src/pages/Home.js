630df8d6f8364883341718db3640b7398ed06932
Mikolaj <m.lewandowski.0231@gmail.com>
2021-11-13 14:15:03 +0100

    set up redux and fetch the games api

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "actions/gamesAction";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  }, []);
+  return (
+    <>
+      <h1>home</h1>
+    </>
+  );
+};
+
+export default Home;
